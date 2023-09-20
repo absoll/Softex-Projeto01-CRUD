883f1c8df8ad032a07ed3ca507da41208b90969f
Matheus Santana <83383321+matheusfsantana@users.noreply.github.com>
2023-09-20 00:08:42 +0000

    Adicionando Promises para funções do Banco

--- banco/banco.js
+++ banco/banco.js
@@ -1,109 +1,127 @@
 const sqlite3 = require('sqlite3').verbose();
 
 function abrirConexao() {
-    const db = new sqlite3.Database('./banco/livros.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
-        if (err) {
-            console.error(err.message);
-        } else {
-            //console.log('Connected to the livros database.');
-        }
-    });
-
-    return db;
-}
-
-function criarTabelas(){
-    const db = abrirConexao();
-
-    db.run('CREATE TABLE IF NOT EXISTS livros(matricula INTEGER PRIMARY KEY, nome TEXT, editora TEXT , autor TEXT)', function(err) {
-        if (err) {
-            return console.log(err.message);
-        }
-    });
-        
-
-    db.close();
-}
-
-
-function inserirLivro(nome,editora,autor){
-
-    const db = abrirConexao();
-  
-    db.run(`INSERT INTO livros(nome,editora,autor) VALUES(?,?,?)`, [nome,editora,autor], function(err) {
-      if (err) {
-        return console.log(err.message);
-      }
-      console.log(`Livro ${nome} inserido com sucesso!`);
-    });
-  
-    db.close();
-  
-}
-
-function listarLivros(){
-    const db = abrirConexao();
-
-    let sql = `SELECT * FROM livros`;
-
-    db.all(sql, [], (err, livros) => {
-        if (err) {
-            throw err;
-        }
-        livros.forEach((livro) => {
-            console.log(livro.matricula,livro.nome,livro.autor,livro.editora);
-        });
-    });
-
-    db.close();
-}
-
-function buscarPorMatricula(matricula){
-    const db = abrirConexao();
-    let sql = `SELECT * FROM livros
-                WHERE matricula  = ?`
-
-    db.get(sql, [matricula], (err, livro) => {
+    return new Promise((resolve, reject) => {
+      const db = new sqlite3.Database('./banco/livros.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
         if (err) {
           console.error(err.message);
+          reject(err);
+        } else {
+          console.log('Connected to the livros database.');
+          resolve(db);
         }
-        console.log(livro.matricula, livro.nome, livro.autor, livro.editora);
-      
       });
-
-    db.close();
-}
-
-function alterarLivro(matricula,nome,autor,editora){
-    const db = abrirConexao();
-    let dadosAtualizados = [nome,autor,editora, matricula];
-    let sql = `UPDATE livros
-                SET nome = ?,
-                autor = ?,
-                editora = ?
-                WHERE matricula = ?`;
-
-    db.run(sql, dadosAtualizados, function(err) {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log(`Livro ${nome} atualizado com sucesso!`);
-
     });
-}
-
-function apagarLivro(matricula){
-    const db = abrirConexao();
-    let sql = `DELETE FROM livros WHERE matricula = ?`;
-
-    db.run(sql, [matricula], function(err) {
+  }
+  
+  // Função para criar tabelas no banco de dados
+  function criarTabelas(db) {
+    return new Promise((resolve, reject) => {
+      db.run('CREATE TABLE IF NOT EXISTS livros(matricula INTEGER PRIMARY KEY, nome TEXT, editora TEXT , autor TEXT)', function(err) {
         if (err) {
-            return console.error(err.message);
+          console.error(err.message);
+          reject(err);
+        } else {
+          console.log('Tabela de livros criada com sucesso.');
+          resolve();
         }
-        console.log(`Livro apagado com sucesso!`);
-
+      });
     });
-}
+  }
+  
+  // Função para inserir um livro no banco de dados
+  function inserirLivro(db, nome, editora, autor) {
+    return new Promise((resolve, reject) => {
+      db.run(`INSERT INTO livros(nome, editora, autor) VALUES (?, ?, ?)`, [nome, editora, autor], function(err) {
+        if (err) {
+          console.error(err.message);
+          reject(err);
+        } else {
+          console.log(`Livro ${nome} inserido com sucesso!`);
+          resolve();
+        }
+      });
+    });
+  }
+
+
+  function listarLivros(db) {
+    return new Promise((resolve, reject) => {
+      let sql = `SELECT * FROM livros`;
+  
+      db.all(sql, [], (err, livros) => {
+        if (err) {
+          console.error(err.message);
+          reject(err);
+        } else {
+          livros.forEach((livro) => {
+            console.log(livro.matricula, livro.nome, livro.autor, livro.editora);
+          });
+          resolve();
+        }
+      });
+    });
+  }
+  
+  // Função para buscar um livro por matrícula no banco de dados
+  function buscarPorMatricula(db, matricula) {
+    return new Promise((resolve, reject) => {
+      let sql = `SELECT * FROM livros WHERE matricula  = ?`;
+  
+      db.get(sql, [matricula], (err, livro) => {
+        if (err) {
+          console.error(err.message);
+          reject(err);
+        } else {
+          if (livro) {
+            console.log(livro.matricula, livro.nome, livro.autor, livro.editora);
+          } else {
+            console.log('Livro não encontrado.');
+          }
+          resolve();
+        }
+      });
+    });
+  }
+  
+  // Função para alterar um livro no banco de dados
+  function alterarLivro(db, matricula, nome, autor, editora) {
+    return new Promise((resolve, reject) => {
+      let dadosAtualizados = [nome, autor, editora, matricula];
+      let sql = `UPDATE livros
+                 SET nome = ?,
+                 autor = ?,
+                 editora = ?
+                 WHERE matricula = ?`;
+  
+      db.run(sql, dadosAtualizados, function(err) {
+        if (err) {
+          console.error(err.message);
+          reject(err);
+        } else {
+          console.log(`Livro ${nome} atualizado com sucesso!`);
+          resolve();
+        }
+      });
+    });
+  }
+  
+  // Função para apagar um livro do banco de dados
+  function apagarLivro(db, matricula) {
+    return new Promise((resolve, reject) => {
+      let sql = `DELETE FROM livros WHERE matricula = ?`;
+  
+      db.run(sql, [matricula], function(err) {
+        if (err) {
+          console.error(err.message);
+          reject(err);
+        } else {
+          console.log(`Livro apagado com sucesso!`);
+          resolve();
+        }
+      });
+    });
+  }
 
 module.exports = {
     abrirConexao,
@@ -114,5 +132,3 @@ module.exports = {
     alterarLivro,
     apagarLivro
   };
-
-listarLivros();
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-
 const leitor = require("readline-sync");
-const db = require('./banco/banco.js');
+const database = require('./banco/banco.js');
+
+
 
 //Função menu - mostra o menu e as opções
 function menu() {
+  //console.clear()
   console.log("----- MENU LIVRARIA -----");
   console.log("1 - Listar todos os livros");
   console.log("2 - Cadastrar livro");
@@ -13,134 +15,82 @@ function menu() {
   console.log("0 - Sair");
 }
 
-async function cadastrarLivro() {
+async function cadastrarLivro(db) {
+
   let nome = leitor.question("Nome do livro: ");
   let editora = leitor.question("Nome da editora: ");
   let autor = leitor.question("Nome do autor: ");
-  
-  await db.inserirLivro(nome, autor, editora);
-  console.log(`Livro "${nome}" cadastrado com sucesso!`);
+  await database.inserirLivro(db,nome,autor,editora);
+  leitor.keyInPause();
+  console.clear();
 }
 
-// Início da execução do código
-(async () => {
+async function main() {
+  let db;
   try {
-    await db.criarTabelas();
-    console.log('Tabelas criadas com sucesso.');
-
-    let opt;
-    do {
-      menu();
-      opt = leitor.questionInt("Escolha uma opção\n");
-      switch (opt) {
-        case 0:
-          console.log("Saindo do sistema...");
-          break;
-        case 1:
-          console.log("----- LISTANDO TODOS OS LIVROS ----");
-          const livros = await db.listarLivros();
-          livros.forEach((livro) => {
-            console.log(livro.matricula, livro.nome, livro.autor, livro.editora);
-          });
-          console.log("Aperte qualquer tecla para voltar ao menu...");
-          leitor.keyIn();
-          break;
-        case 2:
-          await cadastrarLivro();
-          break;
-        // Restante das opções do menu...
-        default:
-          console.log("Opção inválida... digite novamente");
-      }
-    } while (opt !== 0);
-  } catch (error) {
-    console.error('Erro:', error);
-  } finally {
-    // Certifique-se de fechar a conexão com o banco de dados ao final do programa.
-    db.abrirConexao().close();
+    db = await database.abrirConexao();
+    await database.criarTabelas(db);
+  } catch (err) {
+    console.error('Erro ao abrir conexão ou criar tabelas:', err);
+    return;
   }
-})();
 
-// const leitor = require("readline-sync");
-// const db = require('./banco/banco.js');
+  let opt;
+  do {
+    menu();
+    opt = leitor.questionInt("Escolha uma opção\n");
+    switch (opt) {
+      case 0:
+        console.log("Saindo do sistema...");
+        break;
+      case 1:
+        console.log("----- LISTANDO TODOS OS LIVROS ----");
+        await database.listarLivros(db);
+        console.log("Aperte qualquer tecla para voltar ao menu...");
+        leitor.keyIn();
+        console.clear();
+        break;
+      case 2:
+        await cadastrarLivro(db);
+        break;
+      case 3:
+        let matricula = leitor.questionInt(
+          "Digite o numero de matricula do livro a ser buscado: \n"
+        );
+        await database.buscarPorMatricula(db, matricula);
+        console.log("Aperte qualquer tecla para voltar ao menu...");
+        leitor.keyIn();
+        console.clear();
+        break;
+      case 4:
+        let idAlterarLivro = leitor.questionInt("Digite o numero de matricula do livro a ser alterado: \n");
+        let nome = leitor.question("Nome do livro: ");
+        let editora = leitor.question("Nome da editora: ");
+        let autor = leitor.question("Nome do autor: ");
+        await database.alterarLivro(db, idAlterarLivro, nome, editora, autor);
+        console.log("Aperte qualquer tecla para voltar ao menu...");
+        leitor.keyIn();
+        console.clear();
+        break;
+      case 5:
+        let id_remover_livro = leitor.questionInt("Digite o numero de matricula do livro a ser removido: \n");
+        await database.apagarLivro(db, id_remover_livro);
+        console.log("Aperte qualquer tecla para voltar ao menu...");
+        leitor.keyIn();
+        console.clear();
+        break;
+      default:
+        console.log("Opção inválida... digite novamente");
+    }
+  } while (opt != 0);
 
-// db.criarTabelas();
+  db.close((err) => {
+    if (err) {
+      console.error('Erro ao fechar a conexão:', err.message);
+    } else {
+      console.log('Conexão com o banco de dados fechada.');
+    }
+  });
+}
 
-
-// //Função menu - mostra o menu e as opções
-// function menu() {
-//   //console.clear()
-//   console.log("----- MENU LIVRARIA -----");
-//   console.log("1 - Listar todos os livros");
-//   console.log("2 - Cadastrar livro");
-//   console.log("3 - Buscar livro");
-//   console.log("4 - Alterar livro");
-//   console.log("5 - Apagar livro");
-//   console.log("0 - Sair");
-// }
-
-// function cadastrarLivro() {
-
-//   let nome = leitor.question("Nome do livro: ");
-//   let editora = leitor.question("Nome da editora: ");
-//   let autor = leitor.question("Nome do autor: ");
-//   db.inserirLivro(nome,autor,editora);
-//   leitor.keyInPause;
-//   console.clear();
-// }
-
-// //Inicio da execução do codigo
-// let resultado;
-// let opt;
-// do {
-//   menu();
-//   opt = leitor.questionInt("Escolha uma opção\n");
-//   switch (opt) {
-//     case 0:
-//       console.log("Saindo do sistema...");
-//       break;
-//     case 1: //Opção para listar todos os livros
-//       console.log("----- LISTANDO TODOS OS LIVROS ----");
-//       db.listarLivros();
-//       console.log("Aperte qualquer tecla para voltar ao menu...");
-//       leitor.keyIn(); //Fica aguardando o usuario digitar qualquer tecla para prosseguir
-//       console.clear();
-//       break;
-//     case 2: cadastrarLivro();
-//       break;
-//     case 3: // opção para buscar apenas 1 elemento por ID
-//       let matricula = leitor.questionInt(
-//         "Digite o numero de matricula do livro a ser buscado: \n"
-//       );
-
-//       db.buscarPorMatricula(id);
-
-//       console.log("Aperte qualquer tecla para voltar ao menu...");
-//       leitor.keyIn();
-//       console.clear();
-//       break;
-//     case 4: //alterarLivro(id)
-//       let idAlterarLivro = leitor.questionInt("Digite o numero de matricula do livro a ser alterado: \n");
-//       let nome = leitor.question("Nome do livro: ");
-//       let editora = leitor.question("Nome da editora: ");
-//       let autor = leitor.question("Nome do autor: ");
-
-//       db.alterarLivro(idAlterarLivro,nome,editora,autor);
-
-//       console.log("Aperte qualquer tecla para voltar ao menu...");
-//       leitor.keyIn();
-//       console.clear();
-//       break;
-//     case 5: //apagarLivro(id)
-//       let id_remover_livro = leitor.questionInt("Digite o numero de matricula do livro a ser removido: \n");
-
-//       db.apagarLivro(id_remover_livro)
-
-//       console.log("Aperte qualquer tecla para voltar ao menu...");
-//       leitor.keyIn();
-//       console.clear();
-//       break;
-//     default:
-//       console.log("Opção invalida... digite novamente");
-//   }
-// } while (opt != 0);
+main();
